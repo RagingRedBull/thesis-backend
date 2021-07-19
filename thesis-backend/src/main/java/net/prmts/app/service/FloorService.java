e0ece379a087d3fa9e59c1f918370c1176e2f71e
Marcelo Magday <marcelomagday@gmail.com>
2021-07-19 12:06:29 +0800

    Floor Service Cleanup
    Added cleanup for the Temporary File in saveToImageServer method.

--- thesis-backend/src/main/java/net/prmts/app/service/FloorService.java
+++ thesis-backend/src/main/java/net/prmts/app/service/FloorService.java
@@ -3,6 +3,9 @@ package net.prmts.app.service;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,13 +14,14 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.prmts.app.model.Floor;
 import net.prmts.app.model.dto.FloorDto;
@@ -25,6 +29,7 @@ import net.prmts.app.repository.FloorRepository;
 
 @Service
 public class FloorService {
+	private Logger logger = LoggerFactory.getLogger(FloorService.class);
 	private String imgStorageBaseUrl;
 	private FloorRepository floorRepository;
 	private RestTemplate imgStorageRequest;
@@ -39,19 +44,26 @@ public class FloorService {
 	}
 	public ResponseEntity<FloorDto> saveToImageServer(MultipartFile floorImgFile) {
 		String imgStorageUrl = imgStorageBaseUrl + ":8080/images/map/upload";
-		System.out.println("IMG URL: " + imgStorageBaseUrl);
+		this.logger.info("IMG URL: " + imgStorageBaseUrl);
 		// Set Header
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 		// Set Body
 		MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-		body.add("mapFile", new FileSystemResource(convert(floorImgFile)));
+		FileSystemResource floorImgFSR = new FileSystemResource(convert(floorImgFile));
+		body.add("mapFile", floorImgFSR);
 		// Prepare Request
 		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(body,
 				headers);
 		ResponseEntity<FloorDto> response = imgStorageRequest.postForEntity(imgStorageUrl, requestEntity, FloorDto.class);
 		// Save URL on Image Server to DB
 		this.floorRepository.save(new Floor(response.getBody()));
+		// Clean up
+		if(deleteFileSystemResource(floorImgFSR)) {
+			this.logger.info("Temporary FSR has been deleted");
+		} else {
+			this.logger.info("File was not deleted");
+		}
 		return response;
 	}
 
@@ -60,7 +72,7 @@ public class FloorService {
 	 * https://stackoverflow.com/questions/52152337/rest-api-call-other-rest-api-in-
 	 * spring-boot-not-working-for-multipart-file
 	 */
-	public static File convert(MultipartFile file) {
+	private static File convert(MultipartFile file) {
 		File convFile = new File(file.getOriginalFilename());
 		try {
 			convFile.createNewFile();
@@ -68,10 +80,20 @@ public class FloorService {
 			fos.write(file.getBytes());
 			fos.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		return convFile;
+	}
+	
+	private static boolean deleteFileSystemResource(FileSystemResource fsr) {
+		boolean isFileDeleted = false;
+		try {
+			Path tempFile = Paths.get(fsr.getURI());
+			isFileDeleted = Files.deleteIfExists(tempFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return isFileDeleted;
 	}
 }
