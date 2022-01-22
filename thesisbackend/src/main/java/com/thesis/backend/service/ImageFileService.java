14d5483b999cb0fe4c7b643ba5581ec2989b664a
Marcelo Magday <marcelomagday@gmail.com>
2022-01-23 02:37:12 +0800

    Changed logic to save file

--- thesisbackend/src/main/java/com/thesis/backend/service/ImageFileService.java
+++ thesisbackend/src/main/java/com/thesis/backend/service/ImageFileService.java
@@ -1,20 +1,26 @@
 package com.thesis.backend.service;
 
+import com.thesis.backend.controller.FloorController;
 import com.thesis.backend.service.interfaces.FileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 
 @Service
 public class ImageFileService implements FileService {
-
+    private final Logger logger = LoggerFactory.getLogger(ImageFileService.class);
     @Override
     public Resource load(String fileName) throws FileNotFoundException {
         String path = "/var/lib/prmts/images/" + fileName;
@@ -28,9 +34,10 @@ public class ImageFileService implements FileService {
 
     @Override
     public String save(MultipartFile file) throws IOException {
-        String path = "/var/lib/prmts/images/";
-        Files.copy(file.getInputStream(), Paths.get(path), StandardCopyOption.REPLACE_EXISTING);
-        path = path + file.getOriginalFilename();
-        return path;
+        Path saveDir = Paths.get("/var/lib/prmts/images/");
+        Files.copy(file.getInputStream(), saveDir.resolve(Objects.requireNonNull(file.getOriginalFilename())),
+                StandardCopyOption.REPLACE_EXISTING);
+        logger.info("Image at: " + saveDir.toString());
+        return saveDir +file.getOriginalFilename();
     }
 }
