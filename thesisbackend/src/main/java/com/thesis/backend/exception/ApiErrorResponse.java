ddc3f55feb78a6418b25793cae89dceeb158cdf4
Marcelo Magday <61895080+RagingRedBull@users.noreply.github.com>
2022-01-29 23:26:42 +0800

    Ignore Null Fields of ApiErrorResponse

--- thesisbackend/src/main/java/com/thesis/backend/exception/ApiErrorResponse.java
+++ thesisbackend/src/main/java/com/thesis/backend/exception/ApiErrorResponse.java
@@ -1,11 +1,13 @@
 package com.thesis.backend.exception;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiErrorResponse {
     private HttpStatus status;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
