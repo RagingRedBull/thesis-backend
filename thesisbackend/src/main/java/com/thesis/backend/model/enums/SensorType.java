090b24785fe886a7a77afab6376bbc3bc75aa9a7
Marcelo Magday <61895080+RagingRedBull@users.noreply.github.com>
2022-02-28 16:34:13 +0800

    AttrivuteConverter

--- thesisbackend/src/main/java/com/thesis/backend/model/enums/SensorType.java
+++ thesisbackend/src/main/java/com/thesis/backend/model/enums/SensorType.java
@@ -3,7 +3,7 @@ package com.thesis.backend.model.enums;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum SensorType {
-    DHT("DHT"), MQ("MQ"), SOUND("SOUND"), FLAME("FLAME");
+    DHT("DHT"), MQ("MQ"), SOUND("SOUND"), FIRE("FIRE");
     private final String type;
 
     SensorType(String type) {
