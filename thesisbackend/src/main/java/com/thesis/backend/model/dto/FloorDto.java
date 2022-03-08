6f69f7bcc92ebafa2cafd7a3178259372d27080d
Marcelo Magday <61895080+RagingRedBull@users.noreply.github.com>
2022-03-08 17:20:15 +0800

    Include 0 in position

--- thesisbackend/src/main/java/com/thesis/backend/model/dto/FloorDto.java
+++ thesisbackend/src/main/java/com/thesis/backend/model/dto/FloorDto.java
@@ -10,7 +10,6 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 @Setter
 @ToString
-@JsonInclude(value = JsonInclude.Include.NON_DEFAULT)
 public class FloorDto {
     private int id;
     @JsonProperty(required = true)
