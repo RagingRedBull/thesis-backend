8cf1069ddd7d03f9e191592140225b2a190f66dc
Marcelo Magday <61895080+RagingRedBull@users.noreply.github.com>
2022-03-11 14:29:01 +0800

    Changed WebSecConfig

--- thesisbackend/src/main/java/com/thesis/backend/config/WebSecurityConfig.java
+++ thesisbackend/src/main/java/com/thesis/backend/config/WebSecurityConfig.java
@@ -56,7 +56,7 @@ public class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.DELETE).authenticated()
                 .antMatchers(HttpMethod.PUT).authenticated()
                 .antMatchers(HttpMethod.PATCH).authenticated()
-                .antMatchers(HttpMethod.POST, "/detector/**", "/floor/**", "/images/**").authenticated()
+                .antMatchers(HttpMethod.POST, "/compartment/**", "/floor/**", "/images/**").authenticated()
                 .antMatchers("/test/**").authenticated();
     }
 }
