af488d49f6055d7aac7d9f305c454e830e986b6d
Marcelo Magday <61895080+RagingRedBull@users.noreply.github.com>
2022-03-13 17:17:57 +0800

    Bump Sound Sensor Log Threshold to 180

--- thesisbackend/src/main/java/com/thesis/backend/service/SensorLogService.java
+++ thesisbackend/src/main/java/com/thesis/backend/service/SensorLogService.java
@@ -108,7 +108,7 @@ public class SensorLogService implements EntityService<SensorLog, SensorLogDto, 
                     break;
                 }
             } else if (log instanceof SoundSensorLog) {
-                if (((SoundSensorLog) log).getSound() >= 135) {
+                if (((SoundSensorLog) log).getSound() >= 180) {
                     isAbnormal = true;
                     break;
                 }
