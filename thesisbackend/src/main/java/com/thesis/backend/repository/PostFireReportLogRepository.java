16d278590b31fe9baba800f467301af5bfa96062
Marcelo Magday <61895080+RagingRedBull@users.noreply.github.com>
2022-03-14 15:11:31 +0800

    DONT INCLUDE NULL PFR

--- thesisbackend/src/main/java/com/thesis/backend/repository/PostFireReportLogRepository.java
+++ thesisbackend/src/main/java/com/thesis/backend/repository/PostFireReportLogRepository.java
@@ -10,10 +10,11 @@ import java.util.List;
 
 @Repository
 public interface PostFireReportLogRepository extends JpaRepository<PostFireReportLog, Long> {
-    @Query(value = "SELECT new com.thesis.backend.model.dto.PostFireReportLogDto(pfr.id,pfr.timeOccurred,pfr.fireOut) FROM PostFireReportLog pfr")
+    @Query(value = "SELECT new com.thesis.backend.model.dto.PostFireReportLogDto(pfr.id,pfr.timeOccurred,pfr.fireOut) FROM PostFireReportLog pfr " +
+            " WHERE pfr.fireOut IS NOT NULL")
     List<PostFireReportLogDto> getIdAndDates();
     @Query(value = "SELECT pfr.id FROM post_fire_report pfr " +
-            "WHERE pfr.fire_out IS NULL " +
+            "WHERE pfr.fire_out IS NOT NULL " +
             "ORDER BY pfr.time_occurred DESC " +
             "LIMIT 1", nativeQuery = true)
     Long getIdOfLatestPfrWithNoFireOut();
